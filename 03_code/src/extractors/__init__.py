37e8982504bc5c05f959b84c915c9b3e46aceca2
Vanshika Agarwal <avanshika17@gmail.com>
2026-04-19 04:03:47 +0530

    Initialize extractors module with imports

---
+++ 03_code/src/extractors/__init__.py
@@ -0,0 +1,12 @@
+from .arcface import ArcFaceIdentityExtractor
+from .bfm import BFMExtractor
+from .clip import CLIPDisentangler
+from .segmentor import LiteFaceSegmentor
+ 
+__all__ = [
+    "ArcFaceIdentityExtractor",
+    "BFMExtractor",
+    "CLIPDisentangler",
+    "LiteFaceSegmentor",
+]
+ 
